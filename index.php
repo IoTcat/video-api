dd57cceab9cb5d464028475177121b08ff4c056c
cn.yimian.xyz <i@iotcat.me>
2020-07-08 15:21:53 +0800

    ini

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+include './functions.php';
+
+header('Access-Control-Allow-Origin:*');
+
+
+
+header('content-type: video/mp4');
+
+$path = $_REQUEST['path'];
+
+
+if($path){
+
+    returnVideo($path);
+}
+
+
+yimian__log("log_api", array("api" => "video", "timestamp" => date('Y-m-d H:i:s', time()), "ip" => ip2long(getIp()), "_from" => get_from(), "content" => $path)); 
+
+
+
+
+function returnVideo($path){
+    $url = getVideo($path);
+    header("Location: $url");
+}
